b1a10715d8c3e45497aad39319f61ca1cb5741a4
Samadhan Diwane <samadhandiwane@gmail.com>
2021-12-13 16:07:17 +0530

    Added Hystrix implementation in cloud gateway and spring cloud and boot version downgrade

---
+++ cloud-gateway/src/main/java/com/jt/cloud/gateway/controller/FallbackController.java
@@ -0,0 +1,21 @@
+package com.jt.cloud.gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+	@RequestMapping("/orderServiceFallback")
+	public Mono<String> orderServiceFallback() {
+		return Mono.just("Order Service is taking too long to respond or is down. Please try after some time");
+	}
+
+	@RequestMapping("/paymentServiceFallback")
+	public Mono<String> paymentServiceFallback() {
+		return Mono.just("Payment Service is taking too long to respond or is down. Please try after some time");
+	}
+
+}
